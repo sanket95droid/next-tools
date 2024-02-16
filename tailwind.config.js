1270286747a4e011bf017ddd50975790e22feea2
Sanket Zambare <77754719+sanket95droid@users.noreply.github.com>
2024-02-16 16:47:29 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// add this in extend:{ dropShadow: { ....}  }
+
+dropShadow: {
+      glow: [
+        "0 0px 20px rgba(255,255, 255, 0.35)",
+        "0 0px 65px rgba(255, 255,255, 0.2)"
+      ]
+    },
